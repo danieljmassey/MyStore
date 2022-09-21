82c9852588e443e8cefcb800379a56d9616ca8e8
Daniel Massey <dmassey5832@gmail.com>
2022-09-21 11:04:22 -0400

    Remove unused imports

--- src/app/components/product-page/product-item/product-item.component.ts
+++ src/app/components/product-page/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { cartProduct } from 'src/app/models/cartProduct';
 import { Product } from 'src/app/models/product';
 
